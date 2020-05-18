7928be62416411808ea5015fbf8df399074c2afb
Tomasz Kiljańczyk <39602955+Gunock@users.noreply.github.com>
2020-05-18 21:31:26 +0200

    Pushed ddl, removed orm from pom, updated gitignore

---
+++ voip-server/src/main/resources/db/sqlite-ddl.sql
@@ -0,0 +1,32 @@
+-- we don't know how to generate schema main (class Schema) :(
+create table accounts
+(
+    id_account   integer not null
+        constraint accounts_pk
+            primary key autoincrement,
+    username     text    not null,
+    password     text    not null,
+    ipv4_address text
+);
+
+create unique index accounts_id_account_uindex
+    on accounts (id_account);
+
+create unique index accounts_username_uindex
+    on accounts (username);
+
+create table password_policy
+(
+    id_policy    integer not null
+        constraint password_policy_pk
+            primary key autoincrement,
+    policy_name  text    not null,
+    policy_value text
+);
+
+create unique index password_policy_id_policy_uindex
+    on password_policy (id_policy);
+
+create unique index password_policy_policy_name_uindex
+    on password_policy (policy_name);
+
